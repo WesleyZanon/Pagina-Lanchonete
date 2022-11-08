019583aeb6f9e709a9fea55e7e580f588f766a33
WesleyZanon <wesleyzanon17@gmail.com>
2022-11-08 20:27:02 -0300

    update
    Signed-off-by: WesleyZanon <wesleyzanon17@gmail.com>

---
+++ pages/Home/script.js
@@ -0,0 +1,11 @@
+
+
+function calcular(){ 
+    const fone = Number(document.getElementById('fone').value); 
+    const nome = document.getElementById('nome').value
+    const combo = document.getElementById('combo').value
+    const adicionais = document.getElementById(['adicionais']).value
+    const delivery = document.getElementById('delivery').value
+
+    console.log(delivery)
+}
